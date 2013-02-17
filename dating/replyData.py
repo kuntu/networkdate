441af3ebce38adb9efe5afbd1f5316866d9c6e5c
captorxp <captorxp@gmail.com>
2013-02-16 23:56:43 -0500

    add reply preference

---
+++ dating/replyData.py
@@ -0,0 +1,134 @@
+import numpy
+import csv
+from datafeature import * #self defined class
+from collections import Counter
+
+
+def getStrFeatVal(arraydata, featureArray,subfeat):
+	"""
+	arraydata: numpy array of the data
+	featureArray: string array of all the features for the arraydata
+	subfeat: string array of part of the features in the arraydata
+	"""
+	featIdx = [featureArray.index(x) for x in subfeat]
+	tmpArray = list(arraydata[:, featIdx])
+	return ['-'.join(x) for x in tmpArray]
+
+
+
+
+
+def createFeatures(fNameArray, valueInterval=None, valueMin = None):
+	"""
+	create an array of DataFeature objects
+	"""
+	features = []
+	if valueInterval is None:
+		features = [DataFeature(x) for x in fNameArray]
+	else:
+		if valueMin is None:
+			valueMin = [0 for x in fNameArray]
+		
+		for i in xrange(len(fNameArray)):
+			print i, len(fNameArray), len(valueMin), len(valueInterval)
+			features.append( DataFeature(fNameArray[i],valueMin[i],valueInterval[i]))
+	return features
+
+def preprocessDataValue(dataarray,allFeatNameArray, modiFeat):
+	"""
+	This function modify the values of features-column in the dataarray, e.g. the age 18 could be mapped
+	 to type 1, age 24 mapped to type 2 ...
+	 
+	dataarray: multi-dimestion array data
+	allFeatNameArray: a string array containing the names of all features
+	modiFeat: a DataFeature-object array, each element in the array
+	"""
+	newData = None
+	for x in modiFeat:
+		idx = allFeatNameArray.index(x.name)		
+		dataarray[:, idx] = (dataarray[:, idx].astype(int) - x.minVal)/x.intv
+	return dataarray
+
+
+def countCommu(dataarray, feats, sfeats, rfeats):
+	"""
+	refine data
+	1.for each sender, has such following structure:
+		sgroup={ #type of the person who receive a message,  
+		
+				sender1Type={
+					accept: # message reply to the sender who has sender1Type
+					reject: # message didn't reply}
+			}
+	"""
+	#use dictionary {}, similar to js object
+	commu = {}
+	#in the reply, receiver is viewed as sender:
+	sfeaIdx = [feats.index(x) for x in sfeats]
+	rfeaIdx = [feats.index(x) for x in rfeats]
+	repIdx = feats.index("Reply")
+	for i in xrange(len(dataarray)):
+		sgroup = '_'.join(list(dataarray[i][sfeaIdx]))
+		if not sgroup in commu:
+			commu[sgroup] ={}
+		rgroup = '_'.join(list(dataarray[i][rfeaIdx]))		
+		if not rgroup in commu[sgroup]:
+			commu[sgroup][rgroup]={'accept':0.001,'reject':0.001}
+			#commu[sid]['rec'][rid]['rpl'] = 0
+		if dataarray[i][repIdx]=='0':
+			commu[sgroup][rgroup]['reject'] +=1
+		else:
+			commu[sgroup][rgroup]['accept'] +=1
+	return commu
+
+features = []
+with open('../../data/UNI_to_WHOLE.csv', 'rb') as f:
+	mycsv = csv.reader(f)
+	mycsv = list(mycsv)
+	features = [x for x in mycsv[0]]
+
+"""
+sids = getStrFeatVal(mycsv,features,['sender','receiver'])
+countSID = Counter(sids)
+print countSID
+singsid = [x for x in countSID]
+sidCounts = [countSID[x] for x in singsid]
+"""
+
+"""
+set up features here
+"""
+mainfeature = ["age","Weight","Height","PhotoCnt","AnimalSign","NewIncome"]
+sfeature = ['r'+x for x in mainfeature]
+#rfeature = ['r'+x for x in mainfeature]
+rfeature = ["sPhotoCnt","sage","sNewIncome","sWeight"]
+sfeatIdx = [features.index(x) for x in sfeature]
+rfeatIdx = [features.index(x) for x in rfeature]
+#alternative way to set up feature by setting feature value
+
+mycsv = numpy.array(mycsv[1:])
+
+mycsv = preprocessDataValue(mycsv, features,\
+						 createFeatures(["rage", "rWeight", "rHeight", "sage", \
+										"sWeight"],\
+									 [5,10,5,5,10]))
+									 
+#print mycsv[1:200,features.index("Reply")]
+
+cc = countCommu(mycsv, features, sfeature, rfeature)
+keys = [x for x in cc]
+for i in xrange(len(cc)):
+	print keys[i], cc[keys[i]]
+"""
+with open('replydata.csv','wb') as csvfile:
+	for x in cc:
+		row = x+'\t'+cc[x]['group']
+		for i in cc[x]['rec']:
+			row = row +' '+ cc[x]['rec'][i]['group']+':'+("%.4f" % \
+			(cc[x]['rec'][i]['accept']/(cc[x]['rec'][i]['accept']+cc[x]['rec'][i]['reject'])))
+		csvfile.write(row+'\n')
+"""
+def generateCSVData(infile, outfile,givenFields, predictFields,allfields=None):
+	"""
+	generate csv
+	"""
