eeeb26beb2bb4c7af5c21151dbf394aa45c52505
kuntu <captorxp@gmail.com>
2013-04-06 19:26:17 +0800

    add module to create corpus from data

---
+++ sampling/makecorpus.py
@@ -0,0 +1,19 @@
+from allData import *
+
+featsforWords = ['Gender', 'age', 'Height', 'NewDegree', 'PhotoCnt',\
+	'NewIncome', 'NewHousing', 'LoveType']
+sFeatWord = ['s'+x for x in featsforWords]
+rFeatWord = ['r'+x for x in featsforWords]
+allfeats, data = readcsvfile('../../data/receiveMsg.csv')
+#print allfeats
+selected = list(set(sFeatWord) | set(rFeatWord))
+selected.sort()
+selected.insert(len(rFeatWord), 'sender')
+selected.insert(0, 'receiver')
+selected.append('Reply')
+data = subMatrix(data, allfeats, selected)
+print data[0]
+data = preprocessDataValue(data, selected, ['sage', 'sHeight', 'rage', \
+	'rHeight'],	[5, 5, 5, 5], [0, 100, 0, 100])
+#dataToCopus(data, selected, sFeatWord, rFeatWord, '../../data/newCorpus.csv')
+print selected, '\n', data[0]
