005c5df432982b46bc763b45f9ecc2c8d7db3910
Kun <captorxp@gmail.com>
2013-05-07 11:21:59 -0400

    'real random selection for probability of feature select'

--- sampling/dataSta.py
+++ sampling/dataSta.py
@@ -10,7 +10,8 @@ import scipy as sp
 import json
 import os
 import matplotlib
-matplotlib.use('Agg')
+import random as rnd
+#matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 def getFeatDistr(ar, featIdx):
     return collections.Counter(ar[:,featIdx].astype(int))
@@ -69,3 +70,64 @@ def randomSelect(cfgfile=None):
     plt.show()
     #print os.getcwd()
 #randomSelect('../../data/randsel.json')
+
+
+def getMarRplRate(cfgfile=None):
+    inpufile = None
+    outputfile = None
+    selectedFeats = None
+    selectedVals = None
+    featOfDist = None
+    selCondition = False
+    ourdir = '.'
+    if cfgfile is not None:
+        f = open(cfgfile)
+        cfg = json.load(f)
+        print cfg
+        inputfile = cfg['indir']+'/'+cfg['infile']
+        selectedFeats = cfg['selected_feats']
+        selectedVals = cfg['selected_vals']
+        featOfDist = cfg['key_feat']
+        outdir = cfg['outdir']
+        if not os.path.exists(outdir):
+            os.makedirs(outdir)
+        pass
+    allfeats, data = readcsvfile(inputfile)    
+    selFeatIdxes = [allfeats.index(i) for i in selectedFeats]
+    data = selectRowsByVal(data,selFeatIdxes[0],str(selectedVals[0]))
+    selectedData = selectRowsByMultiVal(data,selFeatIdxes,selectedVals)
+    print len(data), len(selectedData)
+    #sp.array([data[i] for i in xrange(len(data)) if data[i][selFeatIdxes[0]]==str(selectedVals[0])])
+    for it in xrange(1): #len(featOfDist)):
+        idx = allfeats.index(featOfDist[it])
+        totalCounts = getFeatDistr(data, idx)
+        totalX = [x for x in totalCounts]
+        totalX.sort()
+        selCounts = getFeatDistr(selectedData, idx)
+        selX = [x for x in selCounts]
+        selX.sort()
+        selYtmp = list([selCounts[x] for x in selX])        
+        xaxis = xrange(totalX[0],totalX[-1]+1)
+        selY = sp.zeros(len(xaxis))
+        print selCounts, totalCounts
+        for x in xrange(len(selX)):
+            print x, selX[x],  selCounts[selX[x]], totalCounts[selX[x]]
+            selY[selX[x]-xaxis[0]] = float(selCounts[selX[x]])/totalCounts[selX[x]]
+        print selY
+        """
+        randY = sp.zeros(len(xaxis))
+        for x in xrange(len(selX)):            
+            selY[selX[x]-xaxis[0]] = selYtmp[x]
+        for x in xrange(len(randX)):
+            randY[randX[x]-xaxis[0]] = randYtmp[x]
+        #yaxis = [counts[x] for x in xaxis]
+        """
+        plt.figure(it)
+        plt.xlabel(featOfDist[it])
+        plt.ylabel('distribution')
+        plt.plot(xaxis, selY, 'r--', label='with pref')
+        #plt.plot(xaxis, randY/randY.sum(),'b-', label='random selection')
+        plt.legend()
+        plt.savefig(outdir+'/'+featOfDist[it]+'.png')
+    plt.show()
+getMarRplRate("../../data/randsel.json")
