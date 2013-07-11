171d642a73ce69ff9e3eaa3ca056d332472e0410
Kun <captorxp@gmail.com>
2013-07-10 22:21:47 -0400

    modify the psmX

--- dataPrepare/Rtools.R
+++ dataPrepare/Rtools.R
@@ -1,4 +1,7 @@
-psmX = c("sage",  "sHeight", "sWeight", "sBodyType", "sBloodType", "sNewOccupation", "sAnimalSign", "sAstrology", "sNewDegree", "sNewIncome", "sNewHousing", "sMarriage",  "sPhysicalLooking", "sLoveType",  "sNewInstitution", "sNewCar", "sPhotoCnt", "rage",  "rHeight", "rWeight", "rBodyType", "rBloodType", "rNewOccupation", "rAnimalSign", "rAstrology", "rNationality", "rReligion", "rNewDegree", "rNewIncome", "rNewHousing", "rMarriage", "rHaveChildren", "rPhysicalLooking", "rNewCar", "rNewProfessional", "rNewLanguage", "rNewNature", "rPhotoCnt",  "distance", "AgeFit", "HeightFit", "IncomeFit", "DegreeFit", "MarriageFit", "HaveChildrenFit", "HouseFit", "LocationFit",  "ageDif", "cityDif", "heightDif", "weightDif", "boddyTypeDif", "bloodTypeDif", "astrologyDif", "degreeDif", "incomeDif", "housingDif", "marriageDif")
+psmX = c("sage",  "sHeight", "sWeight", "sBodyType", "sBloodType", "sNewOccupation", "sAnimalSign", "sAstrology", "sNewDegree", "sNewIncome", "sNewHousing", "sMarriage",  "sPhysicalLooking", "sLoveType",  "sNewCar", "sPhotoCnt", "rage",  "rHeight", "rWeight", "rBodyType", "rBloodType", "rNewOccupation", "rAnimalSign", "rAstrology", "rNationality", "rReligion", "rNewDegree", "rNewIncome", "rNewHousing", "rMarriage", "rHaveChildren", "rPhysicalLooking", "rNewCar", "rNewProfessional",   "rPhotoCnt",  "distance", "AgeFit", "HeightFit", "IncomeFit", "DegreeFit", "MarriageFit", "HaveChildrenFit", "HouseFit", "LocationFit",  "ageDif", "cityDif", "heightDif", "weightDif", "boddyTypeDif", "astrologyDif", "incomeDif", "housingDif", "marriageDif")
 psmD = c("sHaveChildren")
 psMY = c("Reply")
+
+
+
 
